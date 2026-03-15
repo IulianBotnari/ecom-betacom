713eb62ed300640be64d35ffd93d78273301bf2d
IulianBotnari <iulianbotnari93@gmail.com>
2026-03-15 10:53:32 +0100

    First commit

---
+++ src/main/java/com/betacom/EcomBetacomApplication.java
@@ -0,0 +1,13 @@
+package com.betacom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomBetacomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomBetacomApplication.class, args);
+	}
+
+}
