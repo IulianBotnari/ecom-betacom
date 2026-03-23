848e980f6ee1e927252ec3d2d054e800ae3c7218
carlitos-95 <espositocarlos746@gmail.com>
2026-03-23 16:49:01 +0100

    cartTest,CartItemTest

--- src/main/java/com/betacom/controllers/CartController.java
+++ src/main/java/com/betacom/controllers/CartController.java
@@ -27,22 +27,6 @@ import lombok.extern.slf4j.Slf4j;
 public class CartController {
 private final InterfaceCartService cartS;
 	
-//	@PostMapping(path = "create")
-//	public ResponseEntity<Object> create(@Valid @RequestBody CartRequest request) {
-//		Object response = null;
-//		HttpStatus status = HttpStatus.CREATED;
-//		
-//		try {
-//			cartS.create(request);
-//			response = "Creazione avvenuta con successo";
-//		} catch (Exception e) {
-//			status = HttpStatus.BAD_REQUEST;
-//			response = "Errore durnate il salvataggio";
-//		}
-//		return ResponseEntity.status(status).body(response);
-//	}
-	
-	
 	
 	@PutMapping(path = "update")
 	public ResponseEntity<Object> update(@Valid @RequestBody(required = true) CartRequest request){
